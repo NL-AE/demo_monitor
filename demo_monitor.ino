fcf5a7018b1fb2971214da12d5c03f0cc9cfd5d3
Tian-Paddington <58605355+NL-AE@users.noreply.github.com>
2025-08-03 22:42:24 +0100

    using adafruit library and it works but needs to fade in/out

--- demo_monitor.ino
+++ demo_monitor.ino
@@ -2,6 +2,15 @@
 // #include <string.h>
 // #include <math.h>
 
+// Pins
+#define TFT_BL 20
+#define TFT_POW x
+
+// Screen
+#include <TFT_eSPI.h>
+#include "Logo_Bitmap.h"
+TFT_eSPI tft = TFT_eSPI();
+
 // Variables
 enum ScreenState{
   WELCOME,
@@ -29,6 +38,20 @@ volatile int ButOnState, ButUpState, ButDnState;
 void setup(void) {
   // Serial
   Serial.begin(115200);
+  delay(100);
+
+  Serial.println("Demo start");
+
+  // GPIO
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL,0);
+
+  // Display
+  tft.init();
+  tft.fillScreen(TFT_BLACK);
+  tft.setRotation(2);
+
+  digitalWrite(TFT_BL,1);
 
   // Setup
   // attachInterrupt(digitalPinToInterrupt(BUT_ON), ISR_BUT_ON, FALLING);
@@ -38,6 +61,7 @@ void setup(void) {
   // BLE
 
   // Display
+  Serial.println("Setup complete");
 }
 
 /**************************************************************************/
@@ -45,8 +69,22 @@ void setup(void) {
 /**************************************************************************/
 
 void loop(void) {
+
   switch(ScreenState){
     case WELCOME:
+      tft.setTextSize(1);
+      tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+      tft.drawXBitmap(46, 55, logo, logoWidth, logoHeight, TFT_WHITE);
+
+      // tft.drawBitmap(46, 55, LogoBitmap, LogoWid, LogoHei);
+      // tft.setFont(&AvenirNextLTPro_Regular16pt7b);
+      tft.setCursor( 50,230);   tft.print("Welcome");
+      // tft.setFont(&AvenirNextLTPro_Regular8pt7b);
+      tft.setCursor( 43,255);   tft.print("Please connect to the ");
+      tft.setCursor( 30,275);   tft.print("K Plus app to get started");
+
+      // Serial.println("State: WELCOME");
       // // draw screen
       // if(dispDrawn==0){tft.fillScreen(ILI9341_BLACK);}
       // dispDrawn = 1;
@@ -84,6 +122,8 @@ void loop(void) {
     case SETTINGS_COLOR:
       break;
   }
+
+  delay(100);
 }
 
 /**************************************************************************/
